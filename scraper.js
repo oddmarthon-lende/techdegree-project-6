ec97bee411568caab3fcb337fa691f30ca387fd3
Odd Marthon Lende <oddmarthon.lende@me.com>
2018-08-20 15:36:27 +0200

    added comments

--- scraper.js
+++ scraper.js
@@ -6,12 +6,14 @@ const fs      = require('fs');
 const dataDir = './data';
 const baseUrl = 'http://shirts4mike.com/';
 
-
+// Checks the response's statusCode
+// and returns a new Error if not 200 OK
 function checkResponse(error, response) {
   if(!error && response.statusCode !== 200)
     return new Error(`There’s been a ${response.statusCode} error. Cannot connect to ${baseUrl}`);
 }
 
+// Gets a product
 function getProduct(link) {
   return new Promise((resolve, reject) => {
     request(`${baseUrl}${link}`, (error, response, body) => {
@@ -29,6 +31,7 @@ function getProduct(link) {
   });
 }
 
+// Gets the product links
 function getProductLinks() {
   return new Promise((resolve, reject) => {
     request(`${baseUrl}shirts.php`, (error, response, body) => {
@@ -43,12 +46,14 @@ function getProductLinks() {
    });
 }
 
+// Pad a number with zero if the length is less than 2
 function padZero(num) {
   num = '' + num;
   num = num.length < 2 ? '0' + num : num;
   return num;
 }
 
+// Write array of product objects to CSV file
 function writeToCSVFile(products) {
 
   if(!fs.existsSync(dataDir)) {
@@ -70,10 +75,13 @@ function writeToCSVFile(products) {
 
 }
 
+// Writes to the error file
 function writeToErrorFile(error) {
   fs.appendFile('./scraper-error.log', `[${new Date()}] ${error.message}\n`, 'utf8', (error) => error && console.error(error.message));
 }
 
+// Runs the functions above
+// and generates a CSV file with the products
 getProductLinks()
 .then((links => {
   Promise.all(links.map((link) => getProduct(link)))
